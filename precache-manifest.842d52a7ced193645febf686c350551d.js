22ef2cfdbac6b2b899c7e3b45def0f28eaf6f4e6
mcnaveen <funmemcn@gmail.com>
2022-04-05 11:29:03 +0530

    Updates

---
+++ precache-manifest.842d52a7ced193645febf686c350551d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd54f9e87286b0b33c325979a1ca4d84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8862be909cb93716faef",
+    "url": "./static/css/main.3d5a966f.chunk.css"
+  },
+  {
+    "revision": "731df7eba0760d41c23a",
+    "url": "./static/js/2.3d961392.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.3d961392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8862be909cb93716faef",
+    "url": "./static/js/main.f2c1451f.chunk.js"
+  },
+  {
+    "revision": "1261810001eaf93733dc",
+    "url": "./static/js/runtime-main.8beed32d.js"
+  }
+]);
